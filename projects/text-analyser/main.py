f41cb3c2aa2555fe1e7439bccebca14014495156
Abhineetsahay <abhineetsahay@gmail.com>
2025-12-24 01:48:20 +0530

    add a simple project using only runnable

---
+++ projects/text-analyser/main.py
@@ -0,0 +1,47 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+)
+
+parser = StrOutputParser()
+
+sentiment_prompt = PromptTemplate(
+    template="""
+Classify the sentiment of the following text as
+Positive, Neutral, or Negative.
+
+Text: {text}
+""",
+    input_variables=["text"],
+)
+
+summary_prompt = PromptTemplate(
+    template="Provide a brief summary of the following text:\n{text}",
+    input_variables=["text"],
+)
+
+facts_prompt = PromptTemplate(
+    template="Generate 5 important points from the following text:\n{text}",
+    input_variables=["text"],
+)
+
+parallel_chain = RunnableParallel(
+    {
+        "sentiment": sentiment_prompt | model | parser,
+        "brief_idea": summary_prompt | model | parser,
+        "important_points": facts_prompt | model | parser,
+    }
+)
+
+result = parallel_chain.invoke(
+    {"text": "I am very happy as I got placed"}
+)
+
+print(result)
